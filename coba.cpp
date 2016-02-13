08037663cdb8026ccc699c012a08a52411a727b5
Luqman Arifin <l.arifin.siswanto@gmail.com>
2016-02-14 03:52:05 +0700

    Sync 14-02-2016

--- coba.cpp
+++ coba.cpp
@@ -2,19 +2,9 @@
 
 using namespace std;
 
-int n, m;
-char s[(int)1e6 + 5];
+long double a[10];
 
 int main() {
-  int t;
-  scanf("%d", &t);
-  while(t--) {
-    scanf("%d %d %s", &n, &m, s);
-    int ans = 0;
-    for(int i = 1; i < m; i++) {
-      if(s[i] != s[i - 1]) ans++;
-    }
-    cout << ans << endl;
-  }
+  cout << sizeof(long long) << endl;
   return 0;
 }
