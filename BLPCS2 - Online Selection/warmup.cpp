21e6e68be5e1cef6efaddcb69d44b4e8efb28e10
Luqman Arifin <l.arifin.siswanto@gmail.com>
2016-04-03 10:54:30 +0700

    add warmup (nyoba branching)

---
+++ BLPCS2 - Online Selection/warmup.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int a;
+  cin >> a;
+  if(a > 0) {
+    puts("positif");
+  } else if(a == 0) {
+    puts("nol");
+  } else {
+    puts("negatif");
+  }
+  return 0;
+}
