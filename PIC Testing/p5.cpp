2d7d7f24dfb708abda0ce8ace3525ae5a935d41b
Luqman Arifin <t-luqman.arifin@traveloka.com>
2016-09-01 10:55:38 +0700

    PIC testing

---
+++ PIC Testing/p5.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t;
+  while (cin >> t) {
+    if (t == 0) break;
+    if (t == 1) {
+      puts("YC Ganteng");
+    } else if(t == 2) {
+      puts("Andre Ganteng");
+    } else if (t == 3) {
+      puts("Anton Ganteng");
+    } else {
+      puts("Yes PIC");
+    }
+  }
+
+  return 0;
+}
