886a313eae4544618a586b7ee00f72ddcf7ba8a7
Luqman Arifin <l.arifin.siswanto@gmail.com>
2016-10-30 14:54:28 +0700

    final gemastik

---
+++ Final Pemrograman GEMASTIK 2016/c.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t;
+  scanf("%d", &t);
+  while(t--) {
+    long long n, m;
+    scanf("%lld %lld", &n, &m);
+    if (n >= m)
+      puts("0");
+    else {
+      long long ans = 1;
+      for (int i = 1; i <= n; i++) {
+        ans = (ans * i) % m;
+      }
+      printf("%lld\n", ans);
+    }
+
+  }
+  return 0;
+}
