bcfe91052782ae6ac209aef0d4be2f3dcf9ab9c3
Luqman Arifin <l.arifin.siswanto@gmail.com>
2016-11-25 18:10:07 +0700

    ac bellman ford maxflow

--- Codeforces Round #381/e_bellman.cpp
+++ Codeforces Round #381/e_bellman.cpp
@@ -4,73 +4,103 @@ using namespace std;
 
 const int N = 2005;
 const int INF = 2e9 + 1e8;
+#define ll long long
 
-#define MAXN 2010
+/** Max Flow Min Cost **/
+/* complexity: O(min(E^2 V log V, E log V F)) */
+const int maxnodes = 2010;
 
-struct Edge
-{
-    int t, c, w, r;
-    Edge(int _t, int _c, int _w, int _r): t(_t), c(_c), w(_w), r(_r) {};
+int nodes = maxnodes;
+int prio[maxnodes], curflow[maxnodes], prevedge[maxnodes], prevnode[maxnodes], q[maxnodes], pot[maxnodes];
+bool inqueue[maxnodes];
+
+struct Edge {
+  int to, f, cap, cost, rev;
 };
 
+vector<Edge> graph[maxnodes];
 
-int pot[MAXN+5], prv[MAXN+5], dist[MAXN+5], vis[MAXN+5];
-vector<Edge> edge[MAXN+5];
-
-pair<int, int> mcmf(int n, int s, int t)
-{
-    fill(pot, pot+n, 0);
-    int mf = 0, mc = 0;
-    while (true) {
-        fill(dist, dist+n, INF);
-        fill(vis, vis+n, 0);
-        dist[s] = 0;
-        
-        for (int it = 0; it < n; it++) {
-          for (int i = 0; i < n; i++) {
-            for (auto e : edge[i]) {
-              int u = e.t;
-              if (e.c == 0) continue;
-              int ndist = dist[i] + e.w + pot[i]-pot[u];
-              if (ndist < dist[u]) {
-                dist[u] = ndist;
-                prv[u] = e.r;
-              }
-            }
-          }
-        }
-        printf("dist %d\n", dist);
-        
-        int v = t;
-        if (dist[t] == INF) break;
-        int flow = INF;
-        for (int i = 0; i < n; ++i) pot[i] += dist[i];
-        while (v != s) {
-          printf("v %d\n", v);
-            Edge &r = edge[v][prv[v]], &e = edge[r.t][r.r];
-            flow = min(flow, e.c);
-            v = r.t;
-        }
-        mf += flow;
-        v = t;
-        while (v != s) {
-          printf("v %d\n", v);
-            Edge &r = edge[v][prv[v]], &e = edge[r.t][r.r];
-            e.c -= flow;
-            r.c += flow;
-            mc += e.w * flow;
-            v = r.t;
-        }
-    }
-    return make_pair(mf, mc);
-}            
-
-void addEdge(int from, int to, int cap, int cost) {
-  Edge u(to, cap, cost, edge[from].size());
-  Edge v(from, 0, -cost, edge[to].size());
-  edge[from].push_back(u);
-  edge[to].push_back(v);
+void addEdge(int s,int t,int cap,int cost){
+  Edge a ={t,0, cap, cost, graph[t].size()};
+  Edge b ={s,0,0,-cost, graph[s].size()};
+  graph[s].push_back(a);
+  graph[t].push_back(b);
 }
+
+void bellmanFord(int s,int dist[]){
+  fill(dist, dist + nodes,1000000000);
+  dist[s]=0;
+  int qt =0;
+  q[qt++]= s;
+  for(int qh =0;(qh - qt)% nodes !=0; qh++){
+    int u = q[qh % nodes];
+    inqueue[u]=false;
+    for(int i =0; i <(int) graph[u].size(); i++){
+      Edge &e = graph[u][i];
+      if(e.cap <= e.f)continue;
+      int v = e.to;
+      int ndist = dist[u]+ e.cost;
+      if(dist[v]> ndist){
+        dist[v]= ndist;
+        if(!inqueue[v]){
+          inqueue[v]=true;
+          q[qt++% nodes]= v;
+        }
+      }
+    }
+  }
+}
+
+pair<int, int> minCostFlow(int s,int t,int maxf){
+  // bellmanFord can be safely commented if edges costs are non-negative
+  bellmanFord(s, pot);
+  int flow =0;
+  int flowCost =0;
+  while(flow < maxf){
+    priority_queue<ll, vector<ll>, greater<ll>> q;
+    q.push(s);
+    fill(prio, prio + nodes,1000000000);
+    prio[s]=0;
+    curflow[s]=1000000000;
+    while(!q.empty()){
+      ll cur = q.top();
+      int d = cur >>32;
+      int u = cur;
+      q.pop();
+      if(d != prio[u])continue;
+      for(int i =0; i <(int) graph[u].size(); i++){
+        Edge &e = graph[u][i];
+        int v = e.to;
+        if(e.cap <= e.f)continue;
+        int nprio = prio[u]+ e.cost + pot[u]- pot[v];
+        if(prio[v]> nprio){
+          prio[v]= nprio;
+          q.push(((ll) nprio <<32)+ v);
+          prevnode[v]= u;
+          prevedge[v]= i;
+          curflow[v]= min(curflow[u], e.cap - e.f);
+        }
+      }
+    }
+    if(prio[t]==1000000000)break;
+    for(int i =0; i < nodes; i++) pot[i]+= prio[i];
+    int df = min(curflow[t], maxf - flow);
+    flow += df;
+    for(int v = t; v != s; v = prevnode[v]){
+      Edge &e = graph[prevnode[v]][prevedge[v]];
+      e.f += df;
+      graph[v][e.rev].f -= df;
+      flowCost += df * e.cost;
+    }
+  }
+  return make_pair(flow, flowCost);
+}
+
+/* usage example:
+* addEdge (source, target, capacity, cost)
+* minCostFlow(source, target, INF) -><flow, flowCost>
+*/
+
 
 int p[N], u[N];
 
@@ -90,12 +120,12 @@ int main() {
   addEdge(0, n + 1, a, 0);
   addEdge(0, n + 2, b, 0);
   for (int i = 1; i <= n; i++) {
-    addEdge(n + 1, i, 1, p[i]);
-    addEdge(n + 2, i, 1, u[i]);
+    addEdge(n + 1, i, 1, -p[i]);
+    addEdge(n + 2, i, 1, -u[i]);
     addEdge(i, n + 3, 1, 0);
     addEdge(i, n + 3, 1, (int) (1LL * p[i] * u[i] / 1000000));
   }
-  int ans = -mcmf(n + 4, 0, n + 3).second;
+  int ans = -minCostFlow(0, n + 3, INF).second;
   printf("%d.%.6d\n", ans / 1000000, ans % 1000000);
   return 0;
 }
