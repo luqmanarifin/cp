2366703245038720918838cd85fba50a0e8bf90c
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-02-24 22:22:15 +0700

    clean comments

--- Codeforces Round #401/d.cpp
+++ Codeforces Round #401/d.cpp
@@ -14,14 +14,11 @@ int main() {
   for (int i = 0; i < n; i++) {
     cin >> s[i];
     til[i] = s[i].size();
-    //cout << s[i] << endl;
   }
   for (int i = n - 1; i >= 1; i--) {
     int len = min(til[i-1], til[i]); 
-    //printf("%d %d\n", i, len);
     bool same = 1;
     for (int j = 0; j < len; j++) {
-      //printf("%c %c\n", s[i-1][j], s[i][j]);
       if (s[i-1][j] > s[i][j]) {
         til[i-1] = min(til[i-1], j);
         same = 0;
@@ -34,9 +31,7 @@ int main() {
     if (same) {
       til[i-1] = min(til[i-1], til[i]);
     }
-    //printf("\n");
   }
-  //for (int i = 0; i < n; i++) printf("%d ", til[i]); printf("\n");
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < til[i]; j++) printf("%c", s[i][j]);
     printf("\n");
