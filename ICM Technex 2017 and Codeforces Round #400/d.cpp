646a19e074371fe123564410400b055b5ef05f4c
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-02-24 16:06:50 +0700

    upsolve

--- ICM Technex 2017 and Codeforces Round #400/d.cpp
+++ ICM Technex 2017 and Codeforces Round #400/d.cpp
@@ -4,33 +4,52 @@ using namespace std;
 
 const int N = 1e5 + 5;
 
-int a[N];
+int a[N], bef[N];
+
+vector<pair<int, int>> edge[N];
+int col[N];
+
+int dfs(int now, int val) {
+  if (col[now] != -1) {
+    if (col[now] != val) return 1;
+    return 0;
+  }
+  col[now] = val;
+  for (auto it : edge[now]) {
+    if (dfs(it.first, val ^ it.second ^ 1)) {
+      return 1;
+    }
+  }
+  return 0;
+}
 
 int main() {
   int n, m;
   scanf("%d %d", &n, &m);
   for (int i = 1; i <= n; i++) scanf("%d", a + i);
-  for (int i = 0; i < m; i++) {
-    int v;
-    scanf("%d", &v);
-    vector<int> num;
-    while (v--) {
-      int l;
-      scanf("%d", &l);
-      num.push_back(l);
-    }
-    bool ada = 0;
-    for (auto it : num) if (a[it] == 0) ada = 1;
-    if (ada) {
-      for (auto it : num) a[it] ^= 1;
+  for (int i = 1; i <= m; i++) {
+    int len;
+    scanf("%d", &len);
+    while (len--) {
+      int v;
+      scanf("%d", &v);
+      if (bef[v] == 0) {
+        bef[v] = i;
+      } else {
+        edge[i].emplace_back(bef[v], a[v]);
+        edge[bef[v]].emplace_back(i, a[v]);
+      }
     }
   }
-  int ada = 0;
-  for (int i = 1; i <= n; i++) if (a[i] == 0) ada = 1;
-  if (ada) {
-    puts("NO");
-  } else {
-    puts("YES");
+  memset(col, -1, sizeof(col));
+  for (int i = 1; i <= m; i++) {
+    if (col[i] == -1) {
+      if (dfs(i, 0)) {
+        puts("NO");
+        return 0;
+      }
+    }
   }
+  puts("YES");
   return 0;
 }
