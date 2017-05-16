243a4bd36fd25eb3e91d9d9a07450f316e489295
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-05-16 14:57:07 +0700

    upsolve cf

--- Codeforces Round #414/d.cpp
+++ Codeforces Round #414/d.cpp
@@ -6,6 +6,8 @@ const int N = 3e5 + 5;
 
 set<pair<int, int>> edges;
 vector<int> edge[N];
+int ans[N];
+vector<int> all[N];
 
 int main() {
   int n, m;
@@ -18,8 +20,54 @@ int main() {
     edge[u].push_back(v);
     edge[v].push_back(u);
   }
-  int best = 1e9, p = -1
-  for (int i = 0; i < n; i++) 
-
+  int best = 1e9, p = -1;
+  for (int i = 1; i <= n; i++) {
+    if (edge[i].size() < best) {
+      best = edge[i].size();
+      p = i;
+    }
+  }
+  printf("%d\n", p);
+  ans[p] = 1;
+  all[1].push_back(p);
+  queue<int> q;
+  q.push(p);
+  int pol = 1;
+  while (!q.empty()) {
+    int now = q.front();
+    q.pop();
+    for (auto it : edge[now]) {
+      if (!ans[it]) {
+        ans[it] = ans[now] + 1;
+        all[ans[it]].push_back(it);
+        pol = max(pol, ans[it]);
+        q.push(it);
+      }
+    }
+  }
+  /*
+  for (int it = 1; it <= pol; it++) {
+    for (int i = 0; i < all[it].size(); i++) {
+      for (int j = i + 1; j < all[it].size(); j++) {
+        if (!edges.count(make_pair(all[it][i], all[it][j]))) {
+          puts("NO");
+          return 0;
+        }
+      }
+    }
+    if (it > 1) {
+      for (int i = 0; i < all[it].size(); i++) {
+        for (int j = 0; j < all[it-1].size(); j++) {
+          if (!edges.count(make_pair(all[it][i], all[it-1][j]))) {
+            puts("NO");
+            return 0;
+          }
+        }
+      }
+    }
+  }
+  */
+  puts("YES");
+  for (int i = 1; i <= n; i++) printf("%d ", ans[i]);
   return 0;
 }
