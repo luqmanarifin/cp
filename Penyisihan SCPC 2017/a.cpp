c0203c7e59caaf82ea394d45cde263247c94e8a6
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-08-12 15:06:26 +0700

    penyisihan compfest scpc 2017

---
+++ Penyisihan SCPC 2017/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 255;
+
+int a[N][N];
+
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    long long ans = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        scanf("%d", &a[i][j]);
+        ans += a[i][j];
+      }
+    }
+    cout << ans << endl;
+  }
+  return 0;
+}
