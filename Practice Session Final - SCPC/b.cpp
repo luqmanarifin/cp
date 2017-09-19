630d076f5c8d4c87b164f530be24557569ea40ba
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-09-20 00:05:33 +0700

    practice session compfest

---
+++ Practice Session Final - SCPC/b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e5 + 6;
+
+char s[N];
+int cnt[256];
+
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    int n, ans = 0;
+    scanf("%d %s", &n, s);
+    memset(cnt, 0, sizeof cnt);
+    for (int i = 0; i < n; i++) {
+      cnt[s[i]]++;
+    }
+    ans = n;
+    for (int i = 0; i < 256; i++)
+      ans = min(ans, n - cnt[i]);
+    printf("%d\n", ans);
+  }
+
+  return 0;
+}
---
+++ Practice Session Final - SCPC/c.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e5 + 5;
+
+int a[N];
+
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    for (int i = 0; i < n; i++) {
+      scanf("%d", a + i);
+    }
+    sort(a, a + n);
+    int ans = 2e9;
+    int num = n - k;
+    for (int i = 0; i + num - 1 < n; i++) {
+      ans = min(ans, a[i + num - 1] - a[i]);
+    }
+    printf("%d\n", ans);
+  }
+
+  return 0;
+}
