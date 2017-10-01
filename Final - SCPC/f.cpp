662f37d9892c33634d815e1a798b0d0fa42d0f8f
--global <l.arifin.siswanto@gmail.com>
2017-10-02 01:11:45 +0700

    upsolve compfest

--- Final - SCPC/f.cpp
+++ Final - SCPC/f.cpp
@@ -6,73 +6,12 @@ const int MAGIC = 600;
 const int N = 1e5 + 5;
 const long long mod = 1e9 + 8;
 
-long long a[MAGIC], temp[MAGIC][MAGIC], t[MAGIC][MAGIC], ret[MAGIC][MAGIC];
 int nim_pol;
-
-void one() {
-  memset(temp, 0, sizeof(temp));
-  for (int i = 0; i < nim_pol; i++) temp[i][i] = 1;
-}
-
-void temptemp() {
-  memset(ret, 0, sizeof(ret));
-  for (int i = 0; i < nim_pol; i++) {
-    for (int j = 0; j < nim_pol; j++) {
-      for (int k = 0; k < nim_pol; k++) {
-        ret[i][j] += temp[i][k] * temp[k][j];
-        ret[i][j] %= mod;
-      }
-    }
-  }
-  for (int i = 0; i < nim_pol; i++) {
-    for (int j = 0; j < nim_pol; j++) {
-      temp[i][j] = ret[i][j];
-    }
-  }
-}
-
-void ttemp() {
-  memset(ret, 0, sizeof(ret));
-  for (int i = 0; i < nim_pol; i++) {
-    for (int j = 0; j < nim_pol; j++) {
-      for (int k = 0; k < nim_pol; k++) {
-        ret[i][j] += t[i][k] * temp[k][j];
-        ret[i][j] %= mod;
-      }
-    }
-  }
-  for (int i = 0; i < nim_pol; i++) {
-    for (int j = 0; j < nim_pol; j++) {
-      temp[i][j] = ret[i][j];
-    }
-  }
-}
-
-void atemp() {
-  memset(ret, 0, sizeof(ret));
-  for (int j = 0; j < nim_pol; j++) {
-    for (int k = 0; k < nim_pol; k++) {
-      ret[0][j] += a[k] * temp[k][j];
-      ret[0][j] %= mod;
-    }
-  }
-  for (int j = 0; j < nim_pol; j++) {
-    a[j] = ret[0][j];
-  }
-}
-
-void power(int b) {
-  if (b == 0) {
-    one();
-    return;
-  }
-  power(b / 2);
-  temptemp();
-  if (b & 1) ttemp();
-}
 
 vector<int> edge[N];
 int nim[N], cnt[MAGIC];
+
+long long hop[MAGIC][40], ans[MAGIC], tmp[MAGIC];
 
 int main() {
   int tc;
@@ -107,22 +46,39 @@ int main() {
     for (int i = 1; i <= n; i++) {
       cnt[nim[i]]++;
     }
+    memset(hop, 0, sizeof(hop));
     for (int i = 0; i < nim_pol; i++) {
-      a[i] = cnt[i];
-      //if (cnt[i]) printf("%d ada %d\n", i, cnt[i]);
+      hop[i][0] = cnt[i];
     }
-    for (int i = 0; i < nim_pol; i++) {
-      for (int j = 0; j < nim_pol; j++) {
-        int to = j;
-        int from = i;
-        t[i][j] = cnt[from ^ to];
+    for (int bit = 0; bit + 1 < 40; bit++) {
+      for (int i = 0; i < nim_pol; i++) {
+        for (int j = 0; j < nim_pol; j++) {
+          hop[i ^ j][bit + 1] += hop[i][bit] * hop[j][bit];
+          hop[i ^ j][bit + 1] %= mod;
+        }
       }
     }
-    power(k - 1);
-    atemp();
-    long long ans = 0;
-    for (int i = 1; i < nim_pol; i++) ans += a[i];
-    cout << ans % mod << endl;
+    memset(ans, 0, sizeof(ans));
+    ans[0] = 1;
+    for (int bit = 0; bit < 31; bit++) {
+      if (k & (1 << bit)) {
+        memset(tmp, 0, sizeof(tmp));
+        for (int i = 0; i < nim_pol; i++) {
+          for (int j = 0; j < nim_pol; j++) {
+            tmp[i ^ j] += ans[i] * hop[j][bit];
+            tmp[i ^ j] %= mod;
+          }
+        }
+        for (int i = 0; i < nim_pol; i++) {
+          ans[i] = tmp[i];
+        }
+      }
+    }
+    long long tot = 0;
+    for (int i = 1; i < nim_pol; i++) {
+      tot += ans[i];
+    }
+    printf("%lld\n", tot % mod);
   }
 
   return 0;
