7bb412e08d28e0e586d8d2711368370b4f114d6f
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-10-10 16:24:22 +0700

    upsolve xtreme

--- IEEEXtreme 10.0/conjecture.cpp
+++ IEEEXtreme 10.0/conjecture.cpp
@@ -4,48 +4,60 @@ using namespace std;
 
 #define ll long long
 
-vector<ll> A({2, 3, 5, 7, 11, 13, 17, 19, 23});
+vector<long long> A({2, 3, 5, 7, 11, 13, 17, 19, 23});
 // if n < 3,825,123,056,546,413,051, it is enough to test a = 2, 3, 5, 7, 11, 13, 17, 19, and 23.
-
-ll fastexp(ll a, ll b, ll n) { // compute (a ^ b) mod n
-  if (b == 0) return 1 % n;
-  ll tmp = fastexp(a, b / 2, n);
-  tmp = tmp * tmp % n;
-  if (b & 1) {
-    tmp = tmp * a % n;
-  }
-  return tmp;
+long long fastmul(long long a, long long b, long long n) {
+    long long ret = 0;
+    while (b) {
+        if (b & 1)
+            ret = (ret + a) % n;
+        a = (a + a) % n;
+        b >>= 1;
+    }
+    return ret;
 }
 
-bool mrtest(ll n)
+long long fastexp(long long a, long long b, long long n) {//compute (a^b) mod n
+    long long ret = 1;
+    while (b) {
+        if (b & 1)
+            ret = fastmul(ret, a, n);
+        a = fastmul(a, a, n);
+        b >>= 1;
+    }
+    return ret;
+}
+
+bool mrtest(long long n)
 {
     if(n == 1) return false;
-    ll d = n-1;
-    ll s = 0;
+    long long d = n-1;
+    long long s = 0;
     while(d % 2 == 0)
     {
-        s++;
-        d /= 2;
+     s++;
+     d /= 2;
     }
-    for(ll j=0;j<(ll)A.size();j++)
+    for(long long j=0;j<(long long)A.size();j++)
     {
-        if(A[j] > n-1) continue;
-        ll ad = fastexp(A[j], d, n);
-        if(ad % n == 1) continue;
-        bool notcomp = false;
-        for(ll r=0;r<=max(0ll,s-1);r++)
-        {
-            ll rr = fastexp(2,r,n);
-            ll ard = fastexp(ad, rr, n);            
-            if(ard % n == n-1) {notcomp = true; break;}
-        }
-        if(!notcomp)
-        {
-            return false;
-        }
+     if(A[j] > n-1) continue;
+     long long ad = fastexp(A[j], d, n);
+     if(ad % n == 1) continue;
+     bool notcomp = false;
+     for(long long r=0;r<=max(0LL,s-1);r++)
+     {
+       long long rr = fastexp(2,r,n);
+       long long ard = fastexp(ad, rr, n);        
+       if(ard % n == n-1) {notcomp = true; break;}
+     }
+     if(!notcomp)
+     {
+       return false;
+     }
     }
     return true;
 }
+
 
 void brute(long long n) {
   int a = 3;
