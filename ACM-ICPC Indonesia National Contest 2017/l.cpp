f7d336f17ca5795131829d4f44c25e22ba0a54df
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-10-16 17:41:55 +0700

    wiwit upsolve L gg

--- ACM-ICPC Indonesia National Contest 2017/l.cpp
+++ ACM-ICPC Indonesia National Contest 2017/l.cpp
@@ -91,7 +91,7 @@ polynom arith(polynom a, polynom n) {
 
 long long kiriatas(long long r, long long c) {
   int m = min(r, c);
-  if (m == 0)
+  if (m <= 0)
     return 0;
   long long ret = 0;
   // atas
@@ -108,15 +108,16 @@ long long kiriatas(long long r, long long c) {
     rumus = arith(polynom({2, mod-3,4}), polynom({r, 0})); 
     ret += calc(rumus, m+1, c);
   }
+  // cerr << "kiri atas " << r << " " << c << " " << ret << endl;
   return ret;
 }
 long long kananatas(long long r, long long c) {
   int m = min(r, c);
-  if (m == 0)
+  if (m <= 0)
     return 0;
   long long ret = 0;
   // atas
-  polynom rumus = arith(polynom({1, mod-2,4}), polynom({0, 1})); 
+  polynom rumus = arith(polynom({2, mod-1,4}), polynom({0, 1})); 
   ret += calc(rumus, 1, m);
   // kanan
   rumus = arith(polynom({2, 0, 4}), polynom({-1, 1})); 
@@ -129,18 +130,19 @@ long long kananatas(long long r, long long c) {
     rumus = arith(polynom({1-r, 1,4}), polynom({r, 0})); 
     ret += calc(rumus, m+1, c);
   }
+  // cerr << "kanan atas " << r << " " << c << " " << ret << endl;
   return ret;
 }
 long long kiribawah(long long r, long long c) {
   int m = min(r, c);
-  if (m == 0)
+  if (m <= 0)
     return 0;
   long long ret = 0;
   // bawah
   polynom rumus = arith(polynom({2, 3, 4}), polynom({0, 1})); 
   ret += calc(rumus, 1, m);
   // kiri
-  rumus = arith(polynom({1, mod-4,4}), polynom({-1, 1})); 
+  rumus = arith(polynom({2, mod-4,4}), polynom({-1, 1})); 
   ret += calc(rumus, 1, m);
   if (r > c) {
     rumus = arith(polynom({2, 3, 4}), polynom({c, 0})); 
@@ -150,11 +152,12 @@ long long kiribawah(long long r, long long c) {
     rumus = arith(polynom({1-r, mod-3,4}), polynom({r, 0})); 
     ret += calc(rumus, m+1, c);
   }
+  // cerr << "kiri bawah " << r << " " << c << " " << ret << endl;
   return ret;
 }
 long long kananbawah(long long r, long long c) {
   int m = min(r, c);
-  if (m == 0)
+  if (m <= 0)
     return 0;
   long long ret = 0;
   // bawah
@@ -168,107 +171,102 @@ long long kananbawah(long long r, long long c) {
     ret += calc(rumus, m+1, r);
   }
   else if (c > r) {
-    rumus = arith(polynom({2, 1,4}), polynom({r, 0})); 
+    rumus = arith(polynom({2, 1, 4}), polynom({r, 0})); 
     ret += calc(rumus, m+1, c);
   }
+  // cerr << "kanan bawah " << r << " " << c << " " << ret << endl;
   return ret;
 }
 
 int main() {
-  sum[0].x = {0, 1};
-  sum[1].x = {0, (mod + 1) / 2, (mod + 1) / 2};
-  sum[2].x = {0, powmod(6, mod-2), (mod + 1) / 2, powmod(3, mod-2)};
-  sum[3].x = {0, 0, powmod(4, mod-2), (mod+1)/2, powmod(4, mod-2)};
-  sum[4].x = {0, (mod-powmod(30, mod-2)) % mod, powmod(3, mod-2), (mod + 1)/2, powmod(5, mod-2)};
-  sum[5].x = {0, 0, (mod - powmod(12, mod-2)) % mod, 0, (5 * powmod(12, mod-2)) % mod, (mod + 1) / 2, powmod(6, mod-2)};
-  long long n, a, b, c, d;
-  scanf("%lld %lld %lld %lld %lld", &n, &a, &b, &c, &d);
-  cerr << kiriatas(a, b) << " " << kananatas(c, d) << endl;
-  long long mid = n/2 + 1;
-  // cerr << mid << endl;
-  long long ans = 0;
-  if (a <= mid) {
-    if (b <= mid)
-      ans += kiriatas(mid-a, mid-b);
-    else
-      ans -= kananatas(mid-a, b-mid-1);
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    sum[0].x = {0, 1};
+    sum[1].x = {0, (mod + 1) / 2, (mod + 1) / 2};
+    sum[2].x = {0, powmod(6, mod-2), (mod + 1) / 2, powmod(3, mod-2)};
+    sum[3].x = {0, 0, powmod(4, mod-2), (mod+1)/2, powmod(4, mod-2)};
+    sum[4].x = {0, (mod-powmod(30, mod-2)) % mod, powmod(3, mod-2), (mod + 1)/2, powmod(5, mod-2)};
+    sum[5].x = {0, 0, (mod - powmod(12, mod-2)) % mod, 0, (5 * powmod(12, mod-2)) % mod, (mod + 1) / 2, powmod(6, mod-2)};
+    long long n, a, b, c, d;
+    scanf("%lld %lld %lld %lld %lld", &n, &a, &c, &b, &d);
+    long long mid = n/2 + 1;
+    long long ans = 0;
+    if (a <= mid) {
+      if (b <= mid)
+        ans += kiriatas(mid-a, mid-b);
+      else
+        ans -= kananatas(mid-a, b-mid-1);
 
-    if (d <= mid)
-      ans -= kiriatas(mid-a, mid-d-1);
-    else
-      ans += kananatas(mid-a, d-mid);
-  }
-  else {
-    if (b <= mid)
-      ans -= kiribawah(a-mid-1, mid-b);
-    else
-      ans += kananbawah(a-mid-1, b-mid-1);
+      if (d <= mid)
+        ans -= kiriatas(mid-a, mid-d-1);
+      else
+        ans += kananatas(mid-a, d-mid);
+    }
+    else {
+      if (b <= mid)
+        ans -= kiribawah(a-mid-1, mid-b);
+      else
+        ans += kananbawah(a-mid-1, b-mid-1);
 
-    if (d <= mid)
-      ans += kiribawah(a-mid-1, mid-d-1);
-    else
-      ans -= kananbawah(a-mid-1, d-mid);
-  }
-  if (c <= mid) {
-    if (b <= mid)
-      ans -= kiriatas(mid-c-1, mid-b);
-    else
-      ans += kananatas(mid-c-1, b-mid-1);
+      if (d <= mid)
+        ans += kiribawah(a-mid-1, mid-d-1);
+      else
+        ans -= kananbawah(a-mid-1, d-mid);
+    }
+    if (c <= mid) {
+      if (b <= mid)
+        ans -= kiriatas(mid-c-1, mid-b);
+      else
+        ans += kananatas(mid-c-1, b-mid-1);
 
-    if (d <= mid)
-      ans += kiriatas(mid-c-1, mid-d-1);
-    else
-      ans -= kananatas(mid-c-1, d-mid);
-  }
-  else {
-    if (b <= mid)
-      ans += kiribawah(c-mid, mid-b);
-    else
-      ans -= kananbawah(c-mid, b-mid-1);
+      if (d <= mid)
+        ans += kiriatas(mid-c-1, mid-d-1);
+      else
+        ans -= kananatas(mid-c-1, d-mid);
+    }
+    else {
+      if (b <= mid)
+        ans += kiribawah(c-mid, mid-b);
+      else
+        ans -= kananbawah(c-mid, b-mid-1);
 
-    if (d <= mid)
-      ans -= kiribawah(c-mid, mid-d-1);
-    else
-      ans += kananbawah(c-mid, d-mid);
-  } 
-  if (a <= mid && mid <= c && b <= mid && mid <= d) {
-    ans++;
+      if (d <= mid)
+        ans -= kiribawah(c-mid, mid-d-1);
+      else
+        ans += kananbawah(c-mid, d-mid);
+    } 
+    if (a <= mid && mid <= c && b <= mid && mid <= d) {
+      ans++;
+    }
+    if (a <= mid && mid <= c) {
+      if (b < mid && mid < d) {
+        ans += calc(polynom({1, -3, 4}), 1, mid-b);
+        ans += calc(polynom({1, 1, 4}), 1, d-mid);
+      }
+      else if (b < mid) {
+        ans += calc(polynom({1, -3, 4}), max(1LL, mid-d), mid-b);
+      }
+      else if (mid < d) {
+        ans += calc(polynom({1, 1, 4}), max(1LL, b-mid), d-mid);
+      }
+    }
+    if (b <= mid && mid <= d) {
+      if (a < mid && mid < c) {
+        ans += calc(polynom({1, -1, 4}), 1, mid-a);
+        ans += calc(polynom({1, 3, 4}), 1, c-mid);
+      }
+      else if (a < mid) {
+        ans += calc(polynom({1, -1, 4}), max(1LL, mid-c), mid-a);
+      }
+      else if (mid < c) {
+        ans += calc(polynom({1, 3, 4}), max(1LL, a-mid), c-mid);
+      }
+    }
+    ans %= mod;
+    if (ans < 0)
+      ans += mod;
+    printf("%lld\n", ans);
   }
-  if (a <= mid && mid <= c) {
-    if (b < mid && mid < d) {
-      polynom rumus = arith(polynom({1, -3, 4}), polynom({0, 1}));
-      ans += rumus.get(mid-b);
-      rumus = arith(polynom({1, 1, 4}), polynom({0, 1}));
-      ans += rumus.get(d-mid);
-    }
-    else if (b < mid) {
-      polynom rumus = arith(polynom({1, -3, 4}), polynom({0, 1}));
-      ans += rumus.get(mid-b) - rumus.get(mid-d-1);
-    }
-    else if (mid < d) {
-      polynom rumus = arith(polynom({1, 1, 4}), polynom({0, 1}));
-      ans += rumus.get(d-mid) - rumus.get(b-mid-1);   
-    }
-  }
-  if (b <= mid && mid <= d) {
-    if (a < mid && mid < c) {
-      polynom rumus = arith(polynom({1, -1, 4}), polynom({0, 1}));
-      ans += rumus.get(mid-a);
-      rumus = arith(polynom({1, 1, 4}), polynom({0, 1}));
-      ans += rumus.get(c-mid);
-    }
-    else if (a < mid) {
-      polynom rumus = arith(polynom({1, -1, 4}), polynom({0, 1}));
-      ans += rumus.get(mid-a) - rumus.get(mid-c-1);
-    }
-    else if (mid < c) {
-      polynom rumus = arith(polynom({1, 1, 4}), polynom({0, 1}));
-      ans += rumus.get(c-mid) - rumus.get(a-mid-1);   
-    }
-  }
-  ans %= mod;
-  if (ans < 0)
-    ans += mod;
-  printf("%lld\n", ans);
   return 0;
 }
