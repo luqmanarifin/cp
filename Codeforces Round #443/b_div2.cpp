fec935ffa499d9d5ece7be78b8d6edde47a59135
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-10-27 09:36:51 +0700

    upsolve div2

---
+++ Codeforces Round #443/b_div2.cpp
@@ -0,0 +1,89 @@
+/*
+
+Our shadows stretch out on the pavement
+As I walk in the twilight with you
+If we could be together like this forever
+Holding hands
+It's almost enough to make me cry
+
+The wind grows colder
+I can smell winter
+Soon the season will come to this town
+When I can get close to you
+
+This moment
+When the two of us cuddle up
+To gaze at the first snow flower of the year
+Is overflowing with happiness
+
+It's not dependence or weakness
+I just love you
+I thought so with all my heart
+
+I feel like when I'm with you
+I can overcome anything
+I pray that these days
+Will continue forever
+
+The wind rattled the window
+The night shakes you awake
+I will change any sorrow
+Into a smile
+
+The snow flowers fell
+Outside the window
+Unceasing
+And colored our town
+I realized that love means
+Wanting to do something
+For someone else
+
+If I should lose you
+I'll become a star and shine on you
+I'll be with you even on nights
+When your smile is wet with tears
+
+This moment
+When the two of us cuddle up
+To gaze at the first snow flower of the year
+Is overflowing with happiness
+
+It's not dependence or weakness
+I just want to be like this
+With you forever
+I can honestly think that now
+
+The pure white snow flowers
+Bury this town
+Softly drawing memories in our hearts
+Together forever with you...
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 505;
+
+int a[N];
+
+int main() {
+  int n;
+  long long k;
+  scanf("%d %lld", &n, &k);
+  for (int i = 0; i < n; i++) scanf("%d", a + i);
+  int win = 1;
+  int now = max(a[0], a[1]);
+  for (int i = 2; i < n; i++) {
+    if (now > a[i]) {
+      win++;
+    } else {
+      now = a[i];
+      win = 1;
+    }
+    if (a[i] == n || win == k) break;
+  }
+  cout << now << endl;
+  return 0;
+}
