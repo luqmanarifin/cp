65a34485cb92e504bbd6f127e7cacaf41997c78e
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-12-12 10:45:54 +0700

    upsolve d

--- Codeforces Round #450/d_brute.cpp
+++ Codeforces Round #450/d_brute.cpp
@@ -42,49 +42,35 @@ It matters how this ends
 
 using namespace std;
 
-const long long mod = 1e9 + 7;
+const int mod = 1e9 + 7;
+const int N = 155;
 
-long long power(long long a, long long b) {
-  if (b == 0) return 1;
-  long long tmp = power(a, b / 2);
-  tmp = tmp * tmp % mod;
-  if (b & 1) {
-    tmp = tmp * a % mod;
+long long dp[N][N];
+int goal, sumgoal;
+
+long long dfs(int gcd, int sum) {
+  if (gcd % goal) return 0;
+  if (sum == 0) return gcd == goal;
+  if (dp[gcd][sum] != -1) return dp[gcd][sum];
+  dp[gcd][sum] = 0;
+  for (int i = 1; i <= sum; i++) {
+    dp[gcd][sum] += dfs(__gcd(gcd, i), sum - i);
+    dp[gcd][sum] %= mod;
   }
-  return tmp;
+  return dp[gcd][sum];
 }
 
 int main() {
-  int x, y;
-  scanf("%d %d", &x, &y);
-  if (y % x) {
-    puts("0");
-    return 0;
-  }
-  y /= x;
-  x /= x;
-  vector<int> fac;
-  for (int i = 1; i * i <= y; i++) {
-    if (y % i == 0) {
-      fac.push_back(i);
-      if (i * i != y) {
-        fac.push_back(y / i);
-      }
+  for (int i = 1; i <= 100; i++) {
+    goal = i;
+    memset(dp, -1, sizeof(dp));
+    for (int j = 1; j <= 100; j++) {
+      long long ans = 0;
+      sumgoal = j;
+      ans += dfs(0, j);
+      printf("%d %d: %d\n", i, j, (int) ans);
     }
   }
-  sort(fac.begin(), fac.end());
-  vector<int> ans;
-  for (int i = 0; i < fac.size(); i++) {
-    long long now = power(2, fac[i] - 1);
-    for (int j = 0; j < i; j++) {
-      if (fac[i] % fac[j]) continue;
-      now -= ans[j];
-    }
-    now %= mod;
-    if (now < 0) now += mod;
-    ans.push_back(now);
-    //printf("%d: %lld\n", fac[i], now);
-  }
-  cout << ans.back() << endl;
+
   return 0;
 }
