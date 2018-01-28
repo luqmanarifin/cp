f0a7cd26a403651f4eace87a775cb8e03395b478
Luqman Arifin <l.arifin.siswanto@gmail.com>
2018-01-28 13:43:51 +0700

    script

--- 0-unclassified/coba.cpp
+++ 0-unclassified/coba.cpp
@@ -2,23 +2,11 @@
 
 using namespace std;
 
-const int N = 1005;
-
-int nim[N];
-
 int main() {
-  nim[1] = 1;
-  for(int i = 2; i < N; i++) {
-    set<int> s;
-    s.insert(nim[i - 1]);
-    s.insert(nim[i / 2] ^ nim[(i + 1) / 2]);
-    for(int j = 0; j < 10; j++) {
-      if(!s.count(j)) {
-        nim[i] = j;
-        break;
-      }
-    }
-  }
-  for(int i = 0; i < 100; i++) printf("%d : %d\n", i, nim[i]);
+  string a = "aab";
+  do {
+    cout << a << endl;
+  } while (next_permutation(a.begin(), a.end()));
+
   return 0;
 }
