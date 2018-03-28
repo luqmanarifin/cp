7dfe78289160133cb728424e1d3a44cbc7c9d71c
Luqman Arifin <l.arifin.siswanto@gmail.com>
2018-03-28 19:21:28 +0700

    change template neerc11

--- 2011-2012 ACM-ICPC Northeastern European Regional Contest (NEERC 11)/template.cpp
+++ 2011-2012 ACM-ICPC Northeastern European Regional Contest (NEERC 11)/template.cpp
@@ -2,20 +2,8 @@
 
 using namespace std;
 
-const string NAME = "template";
-
 int main() {
-#ifndef LOCAL
-  freopen((NAME + ".in").c_str(), "r", stdin);
-  freopen((NAME + ".out").c_str(), "w", stdout);
-#endif
-  
 
-
-#ifndef LOCAL
-  fclose(stdin);
-  fclose(stdout);
-#endif
 
   return 0;
 }
