68af4f7c23fdcd8d25209b4f8656047eaa78ea71
Luqman Arifin <l.arifin.siswanto@gmail.com>
2018-05-06 18:00:10 +0700

    rewrite p

--- 2017-2018 ACM-ICPC Pacific Northwest Regional Contest/p.cpp
+++ 2017-2018 ACM-ICPC Pacific Northwest Regional Contest/p.cpp
@@ -2,74 +2,6 @@
 
 using namespace std;
 
-vector<long long> A({2, 3, 5, 7, 11, 13, 17, 19, 23});
-// if n < 3,825,123,056,546,413,051, it is enough to test a = 2, 3, 5, 7, 11, 13, 17, 19, and 23.
-
-long long largemul(long long a, long long b, long long n) {
-  // assert(0 <= a && a < n && 0 <= b && b < n);
-  long long r = 0;
-  for (; b; b >>= 1, a <<= 1) {
-    if (a >= n) a -= n;
-    if (b & 1) {
-      r += a;
-      if (r >= n) r -= n;
-    }
-  }
-  return r;
-}
-
-long long fastexp(long long a, long long b, long long n) {
-  // assert(0 <= a && a < n && b >= 0);
-  long long ret = 1;
-  for (; b; b >>= 1, a = largemul(a, a, n))
-    if (b & 1) ret = largemul(ret, a, n);
-  return ret;
-}
-
-bool mrtest(long long n) {
-  if (n == 1) return false;
-  long long d = n-1;
-  int s = 0;
-  while ((d & 1) == 0) {
-    s++;
-    d >>= 1;
-  }
-  s--;
-  if (s < 0) s = 0;
-  for (int j = 0; j < (int)A.size(); j++) {
-    if (A[j] >= n) continue;
-    long long ad = fastexp(A[j], d, n);
-    if (ad == 1) continue;
-    bool notcomp = false;
-    long long a2rd = ad;
-    for (int r = 0; r <= s; r++) {
-      if(a2rd == n-1) {notcomp = true; break;}
-      a2rd = largemul(a2rd, a2rd, n);
-    }
-    if (!notcomp) {
-      return false;
-    }
-  }
-  return true;
-}
-
-long long gcd(long long a, long long b) { return a ? gcd(b % a, a) : b; }
-
-long long pollard_rho(long long n) {
-  int i = 0, k = 2;
-  long long x = 3, y = 3; // random seed = 3, other values possible
-  while (1) {
-    i++;
-    x = largemul(x, x, n)-1; // generating function
-    if (x < 0) x += n;
-    long long d = gcd(llabs(y - x), n); // the key insight
-    if (d != 1 && d != n) return d;
-    if (i == k) y = x, k <<= 1;
-  }
-}
-
-
-
 const int N = 1e6 + 5;
 
 long long ans[N];
@@ -79,16 +11,6 @@ long long last[N];
 vector<long long> factors[N];
 vector<long long> prime;
 
-long long power(long long a, long long b) {
-  if (b == 0) return 1;
-  long long tmp = power(a, b / 2);
-  tmp = tmp * tmp;
-  if (b & 1) {
-    tmp = tmp * a;
-  }
-  return tmp;
-}
-
 long long arith(long long r, long long n) {
   long long ret = 0, now = 1;
   for (int i = 0; i < n; i++) {
@@ -96,12 +18,6 @@ long long arith(long long r, long long n) {
     now *= r;
   }
   return ret;
-}
-
-long long find_prime_factor(long long n) {
-  if (n < N) return last[n];
-  if (mrtest(n)) return n;
-  return find_prime_factor(pollard_rho(n));
 }
 
 long long solve(long long n, long long a) {
