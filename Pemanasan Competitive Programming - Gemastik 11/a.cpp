b82443eefe346639547df6b4a7f49fa4a81854d8
Luqman Arifin <l.arifin.siswanto@gmail.com>
2018-09-25 00:21:02 +0700

    prelim gemastik its

---
+++ Pemanasan Competitive Programming - Gemastik 11/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1005;
+
+string a[N];
+
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    reverse(a[i].begin(), a[i].end());
+  }
+  sort(a, a + n);
+  for (int i = 0; i < n; i++) {
+    reverse(a[i].begin(), a[i].end());
+    printf("%s\n", a[i].c_str());
+  }
+
+  return 0;
+}
