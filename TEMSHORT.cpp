c054ed1b17acd557d91cc7e493b227f43d8981f5
Luqman Arifin Siswanto <luqman.siswanto@shopee.com>
2018-11-18 23:08:10 +0800

    add includes because of clang compiler on mac

--- TEMSHORT.cpp
+++ TEMSHORT.cpp
@@ -1,4 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <cstdio>
+#include <climits>
+#include <vector>
+#include <map>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <utility>
+#include <functional>
+#include <cassert>
 
 using namespace std;
 
