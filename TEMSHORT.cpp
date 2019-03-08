ed48467d33ee6c69d5139f88a1ed97a4cdb86f8d
Luqman Arifin Siswanto <luqman.siswanto@shopee.com>
2019-03-08 18:27:57 +0800

    bring back short template cuz of Mac GCC

--- TEMSHORT.cpp
+++ TEMSHORT.cpp
@@ -1,24 +1,4 @@
-#include <iostream>
-#include <cstdlib>
-#include <fstream>
-#include <cstdio>
-#include <climits>
-#include <vector>
-#include <map>
-#include <list>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <set>
-#include <sstream>
-#include <string>
-#include <cstring>
-#include <algorithm>
-#include <bitset>
-#include <cmath>
-#include <utility>
-#include <functional>
-#include <cassert>
+#include <bits/stdc++.h>
 
 using namespace std;
 
