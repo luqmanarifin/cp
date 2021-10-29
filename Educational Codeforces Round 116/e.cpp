5c32d5af9e5ed139d98bb0a3a82e60480412b6a9
Luqman <luqman.siswanto@shopee.com>
2021-10-30 02:17:03 +0800

    upsolve E

--- Educational Codeforces Round 116/e.cpp
+++ Educational Codeforces Round 116/e.cpp
@@ -2,27 +2,37 @@
 
 using namespace std;
 
-const long long mod = 998244353LL;
+const long long mod = 998244353;
+const int N = 505;
 
-long long power(long long a, long long b) {
-  if (b == 0) return 1;
-  long long ret = power(a, b / 2);
-  ret = ret * ret % mod;
-  if (b & 1) {
-    ret = ret * a % mod;
-  }
-  return ret;
-}
+long long power[N][N], c[N][N];
+long long dp[N][N];
 
 int main() {
-  int n, k;
-  scanf("%d %d", &n, &k);
-  long long ans = power(k, n);
-  for (int x = n; x <= k; x++) {
-    long long cur = n * power(x - 1, n - 1) % mod;
-    ans -= cur;
-    if (ans < 0) ans += mod;
+  for (int i = 1; i < N; i++) {
+    c[i][0] = c[i][i] = 1;
+    for (int j = 1; j < i; j++) {
+      c[i][j] = (c[i-1][j-1] + c[i-1][j]) % mod;
+    }
+    power[i][0] = 1;
+    for (int j = 1; j < N; j++) {
+      power[i][j] = power[i][j-1] * i % mod;
+    }
   }
-  cout << ans << endl;
+
+  int n, x;
+  scanf("%d %d", &n, &x);
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= x; j++) {
+      dp[i][j] = power[min(i-1, j)][i];
+      if (j >= i) {
+        for (int alive = 1; alive <= i; alive++) {
+          dp[i][j] += c[i][alive] * dp[alive][j-i+1] % mod * power[i-1][i-alive] % mod;
+          dp[i][j] %= mod;
+        }
+      }
+    }
+  }
+  cout << dp[n][x] << endl;
   return 0;
 }
