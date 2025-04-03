df5d78e48f246c4df97ae378bff935f05a36bc13
Luqman <luqman.siswanto@shopee.com>
2025-04-03 22:25:07 +0800

    almost upsolve

--- Codeforces Round 1011/e.cpp
+++ Codeforces Round 1011/e.cpp
@@ -7,7 +7,13 @@ const int N = 1e4 + 5;
 int a[N];
 
 int main() {
-
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf
+  }
 
   return 0;
 }
